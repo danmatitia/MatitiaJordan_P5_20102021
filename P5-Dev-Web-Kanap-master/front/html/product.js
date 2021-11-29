0f78039e0ae287c8b3e18f52f98bd13900f5ceca
matitia <jmatitia@gmail.com>
2021-11-29 16:10:19 +0100

    mise en place produit et panier

--- P5-Dev-Web-Kanap-master/front/html/product.js
+++ P5-Dev-Web-Kanap-master/front/html/product.js
@@ -1,10 +1,10 @@
 /** Représentation du format d'un produit*/ 
 /** search params pour manipuler les parametres de la requete URL*/
-let params = new URL(window.location.href).searchParams;
-/** nouvelle URL indiqué par id */
+let params = new URL(document.location).searchParams;
+/** nouvelle URL indiqué par id, pour passer l’id d’une page à une autre */
 let newID = params.get('id');
 
-/** variables prouits */
+/** variables deescriptive des prouits */
 const image = document.getElementsByClassName('item__img');
 const title = document.getElementById('title');
 const price = document.getElementById('price');
@@ -25,7 +25,7 @@ fetch("http://localhost:3000/api/products/" + newID)
     title.innerHTML = `<h1>${data.name}</h1>`;
     price.innerText = `${data.price}`;
     description.innerText = `${data.description}`;
-        for (number in data.colors) { /**choix des couleurs */
+        for (number in data.colors) { /** choix des couleurs */
             colors.options[colors.options.length] = new Option(
             data.colors[number],
             data.colors[number]
@@ -36,7 +36,7 @@ fetch("http://localhost:3000/api/products/" + newID)
         alert('Oops ! Le serveur ne répond pas');
 });
 
-/**Récupération des données clients */
+/** Récupération des données clients */
 /** Nouvelles variables */
 
 const selectQuantity = document.getElementById('quantity');
@@ -57,29 +57,27 @@ addToCart.addEventListener('click', (event) => {
     quantity: selectQuantity.value,
   };
 
-/** Stockage clés + valeurs dans la nouvelles variable locale storage  */
+/** Stockage, clés + valeurs, dans la nouvelles variable localestorage  */
 
 let productInLocalStorage =  JSON.parse(localStorage.getItem('product'));
 
   /**  j'ajoute les produits sélectionnés dans le localStorage */
   const addProductLocalStorage = () => {
-    /** je récupère la sélection de l'utilisateur dans le tableau de l'objet :*/
+    /** je récupère la sélection de l'utilisateur dans le tableau de l'objet */
     /** on peut voir dans la console qu'il y a les données,mais pas encore stockées dans le storage */
     productInLocalStorage.push(selection);
-    // je stocke les données récupérées dans le localStorage :
-    // JSON.stringify permet de convertir les données au format JavaScript en JSON 
-    // vérifier que key et value dans l'inspecteur contiennent bien des données
+    /** stockage des données récupérées dans le localStorage :*/
+    /** vérifier que key et value dans l'inspecteur contiennent bien des données */
     localStorage.setItem('product', JSON.stringify(productInLocalStorage));
     }
-  
-    // je crée une boîte de dialogue pour confirmer l'ajout au panier
+
+    /**  je crée une boîte de dialogue pour confirmer l'ajout au panier */
     let addConfirm = () => {
       alert('Le produit a bien été ajouté au panier');
     }
   
     let update = false;
     
-    // s'il y a des produits enregistrés dans le localStorage
     if (productInLocalStorage) { /** verifier que le produit ne soit pas deja dans le localstorage/panier */ 
      productInLocalStorage.forEach (function (productOk, key) {
       if (productOk.id == newID && productOk.color == selectColors.value) {
@@ -96,9 +94,10 @@ if (!update) {
     }
   }
 
-  // s'il n'y a aucun produit enregistré dans le localStorage 
+  /** s'il n'y a aucun produit enregistré dans le localStorage */
   else {
-    // je crée alors un tableau avec les éléments choisi par l'utilisateur
+    
+    /** création tableau avec les éléments choisi par l'utilisateur */
     productInLocalStorage = [];
     addProductLocalStorage();
     addConfirm();
