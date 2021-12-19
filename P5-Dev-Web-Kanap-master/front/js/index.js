450e6026a045f301a3466333593098010774c5c6
matitia <jmatitia@gmail.com>
2021-12-19 15:45:52 +0100

    new showproduct chargé un par un

--- P5-Dev-Web-Kanap-master/front/js/index.js
+++ P5-Dev-Web-Kanap-master/front/js/index.js
@@ -3,8 +3,11 @@ fetch('http://localhost:3000/api/products')
     /** première promesse .then */
   .then(res => res.json())
     /** deuxième promesse .then qui va afficher les données contenues dans ma fonction showProducts  */
-  .then(data => { 
-    showProducts(data);
+  .then(data => {
+    data.forEach( d => {
+      showProduct(d);
+    }
+    ) 
   })
   /**  Message au cas où le serveur ne répond pas */
 
@@ -14,21 +17,19 @@ fetch('http://localhost:3000/api/products')
    });
 
    /** Fonction permettant d'afficher les produits dans la page d'accueil */
-   function showProducts(data) {
+   function showProduct(data) {
      /** pour ma variable product de ma promise .then data */
-     for (product of data) {
          /**  trouver l'élément #items dans index.html...*/
          const itemCard = document.getElementById('items');
 
          itemCard.innerHTML +=`
-         <a href="./product.html?id=${product._id}">
+         <a href="./product.html?id=${data._id}">
          <article>
-           <img src="${product.imageUrl}" alt="${product.altTxt}">
-           <h3 class="productName">${product.name}</h3>
-           <p class="productDescription">${product.description}</p>
+           <img src="${data.imageUrl}" alt="${data.altTxt}">
+           <h3 class="productName">${data.name}</h3>
+           <p class="productDescription">${data.description}</p>
          </article>
          </a>
-       `; 
-     }
+       `;
  }
             
